c3acd803794d70706b409c20c905cee5b7e18574
Kwan <kopiteken@163.com>
2017-08-09 01:01:20 +0800

    添加单元测试注释

--- tests/MD5HasherTest.php
+++ tests/MD5HasherTest.php
@@ -9,15 +9,36 @@
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class MD5HasherTest
+ */
 class MD5HasherTest extends TestCase
 {
+    /**
+     * @var
+     */
     protected $hasher;
 
+    /**
+     * MD5HasherTest constructor.
+     * @param $hasher
+     */
+    public function __construct($hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    /**
+     * 初始化单元测试类
+     */
     public function setUp()
     {
         $this->hasher = new \Szzmzb\Hasher\MD5Hasher();
     }
 
+    /**
+     * 测试hash生成
+     */
     public function testMD5HasherMake()
     {
         $password = md5('password');
@@ -26,6 +47,9 @@ class MD5HasherTest extends TestCase
         $this->assertEquals($password, $hashPassword);
     }
 
+    /**
+     * 测试加盐hash生成
+     */
     public function testMD5HasherMakeWithSalt()
     {
         $salt = 'salt';
@@ -35,6 +59,9 @@ class MD5HasherTest extends TestCase
         $this->assertEquals($password, $hashPassword);
     }
 
+    /**
+     * 检测生成hash值是否正确
+     */
     public function testMD5HasherCheck()
     {
         $password = md5('password');
